17d0b50e49506a403fb51a0f21d7b5e80b03d687
Sazid-Ah <sazid@sparkuptech.in>
2025-06-02 23:19:05 +0530

    ..

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-const app = require('./app');
+// const app = require('./app');
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const formRoutes = require('./routes/formRoutes');
+const errorHandler = require('./middlewares/errorHandler');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/forms', formRoutes);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8080;
 
