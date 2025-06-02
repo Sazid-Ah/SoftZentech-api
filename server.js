b130b61ba9067de5c657f81b2eca41c84dbc0082
Sazid-Ah <sazid@sparkuptech.in>
2025-06-03 00:11:26 +0530

    ..

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
+const cors = require('cors');
+// ✅ CORS config
+const allowedOrigins = ['http://localhost:80', 'http://softzentech.com'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
