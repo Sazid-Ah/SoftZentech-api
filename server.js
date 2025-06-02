c83bfd5343c2efa24cbcdf2252dde50d08460360
Sazid-Ah <sazid@sparkuptech.in>
2025-06-02 23:21:14 +0530

    ..

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ const errorHandler = require('./middlewares/errorHandler');
 
 dotenv.config();
 connectDB();
-
 const app = express();
 app.use(express.json());
+const PORT = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+})
 app.use('/api/forms', formRoutes);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 8080;
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
